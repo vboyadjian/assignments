7a88a974f638e43e29eeeb234340b835860bd808
Victor <victor_boyadjian@student.hks.harvard.edu>
2019-01-07 17:01:46 -0500

    add comments to ad-comments

--- sayhello.py
+++ sayhello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#this script reads from STDIN and greets the person whose name it gets passed in.
+#this script reads from STDIN and greets     the person whose name it gets passed in.
 import sys
 name = sys.stdin.read()
 print("Hello " + name + "!")
