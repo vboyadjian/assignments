3872bbf579d005b4e27894953d27142f3023742f
Victor <victor_boyadjian@student.hks.harvard.edu>
2019-01-08 18:14:39 -0500

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# This part of the code defines a function for multiplication
+def multiply(a,b):
+    return a * b
+
+    # This part of the code defines a function for division
+def division(a,b):
+    return a / b
+
+
+# This part of the code defines a function for adding
+def adding(a,b):
+    return a + b
+
+# This part of the code defines a function for subtracting
+def subtracting(a,b):
+    return a - b
+
+
+
