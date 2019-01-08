251c4d6776b6c578e5034f9cc7860c67d78837e2
Victor <victor_boyadjian@student.hks.harvard.edu>
2019-01-08 18:52:33 -0500

    added functions square and cube to calculator

--- calculator.py
+++ calculator.py
@@ -2,7 +2,15 @@
 def multiply(a,b):
     return a * b
 
-    # This part of the code defines a function for division
+# This part of the code defines a function for square
+def square(a):
+    return a ** 2
+
+# This part of the code defines a function for cube
+def cube(a):
+    return a ** 3
+
+# This part of the code defines a function for division
 def division(a,b):
     return a / b
 
@@ -18,3 +26,11 @@ def subtracting(a,b):
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+print("I'm going use the calculator functions to square 3)
+y = square(3)
+print(y)
+
+print("I'm going use the calculator functions to multiply 5)
+z = cube(5)
+print(z)
