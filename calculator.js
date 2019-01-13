e6ed99d9976fcbc1458ddaef961f119697e837e1
Victor <victor_boyadjian@student.hks.harvard.edu>
2019-01-13 10:14:49 -0500

    I have made it return a value and print. Works fine

--- calculator.js
+++ calculator.js
@@ -19,4 +19,6 @@ function divide(a,b) {
 var x = multiply(8,9)
 
 // This prints x to the terminal for humans to read
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
 console.log(x)
