582069a9c4ccfe5707ab290c9914e2ac076cb47d
Victor <victor_boyadjian@student.hks.harvard.edu>
2019-01-13 10:12:53 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,22 @@
+// This part of the code defines a function
+function multiply(a,b) { 
+    return a * b
+}
+
+function add(a,b) { 
+    return a * b
+}
+
+function subtract(a,b) { 
+    return a * b
+}
+
+function divide(a,b) { 
+    return a * b
+}
+
+// This part of the code then calls that function and assigns the variable x to its return value
+var x = multiply(8,9)
+
+// This prints x to the terminal for humans to read
+console.log(x)
